42fea7cd5515785c1a34f02f1367671bdcfeadb1
Aryan Singh <ary1733@gmail.com>
2022-11-13 14:16:03 +0530

    psrf bug fix
    Co-authored-by: VINIT RAJ <vinitraj412@gmail.com>
    Co-authored-by: Pranav Rajput <kodekuzuri@users.noreply.github.com>
    Co-authored-by: Abhinandan De <Abhitipu@users.noreply.github.com>

--- Assn3/Assignment3_10_rocchio.py
+++ Assn3/Assignment3_10_rocchio.py
@@ -512,7 +512,7 @@ if __name__ == "__main__":
     # break
     modified_query_tf_idf = {query_id: modify_query(query_tf_idf[query_id],pseudo_feedback[query_id-1], config ) for query_id, query_token_list in query_vectors.items()}  
     ranked_list = get_ranks(modified_query_tf_idf, doc_tf_idf, f"NewRanks_{idx}_ps_relevance.csv")
-    ranked_lists.append(ranked_list)
+    ranked_lists.append([["dummy"]] + ranked_list)
     
 
   # print(modified_query)
